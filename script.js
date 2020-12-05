70c055fe0e09323ba5153d11e352e1fab171e845
Merel Volgers <merelvolgers@gmail.com>
2020-12-05 17:48:19 +0100

    radio buttons added

---
+++ script.js
@@ -0,0 +1,39 @@
+// Requirements 
+// Aan deze eisen moet jouw project voldoen:
+
+// 1.Als gebruiker wil ik een lijst kunnen zien van de beschikbare films met de poster van de film. Check de data: elke film in de database heeft een link naar de juiste poster.
+// 2.Als gebruiker wil ik bovenaan de pagina kunnen klikken op 5 verschillende filters in de vorm van radio-buttons. De filter functionaliteit wordt hieronder verder toegelicht.
+// 3.Als gebruiker kan ik maar 1 filter tegelijk gebruiken.
+// Als ik een ander filter aanklik gaat het andere filter dus weer uit. (Hence de radiobutton (1 antwoord mogelijk), in tegenstelling tot een checkbox (meerdere antwoorden mogelijk).
+// 4.Categorie 1: Als gebruiker wil ik kunnen filteren op de categorie nieuwste films: van de laatste jaren, dat betekent 2014 of nieuwer.
+// 5.Categorie 2: Als gebruiker wil ik kunnen filters op films met "Avengers" in de titel.
+// 6.Categorie 3: Als gebruiker wil ik kunnen filteren op films met "X-Men" in de titel.
+// 7.Categorie 4: Als gebruiker wil ik kunnen filteren op films met "Princess" in de titel.
+// 
+//8.Categorie 5: Als gebruiker wil ik kunnen filteren op films met "Batman" in de titel.
+// Gebruik arraymethods voor je filters
+// Check of een gedeelte van een string in een andere string aanwezig is met de .includes() method.
+// De laatste 4 filters lijken heel veel op elkaar. Heb je daar meerdere functies voor nodig? Kan het ook in 1?
+// 
+// 9.Als gebruiker kan ik op de poster van de film klikken, waardoor ik naar de juiste IMDB pagina wordt gebracht.
+// IMDB werkt met een id per film/serie in de URL. Deze ids vind je ook weer terug in onze filmdatabase. Zie bijvoorbeeld: https://www.imdb.com/title/tt0944947/ Pas de URL van IMDB aan en plak het juiste ID erachter.
+
+
+
+// function check (){
+//     document.getElementById("new").checked = true;
+// }
+
+// function uncheck (){
+//     document.getElementById("new").checked = false;
+// }
+
+console.log(movies);
+
+const movieList = document.getElementById ("movie-list");
+
+const addMoviesToDom = (movies) =>{
+
+}
+
+
