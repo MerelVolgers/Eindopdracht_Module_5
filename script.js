683a77dbf6f57c0bb651b09d111ca6d6700d8dfb
Merel Volgers <merelvolgers@gmail.com>
2020-12-10 12:20:33 +0100

    met werkende film filter

--- script.js
+++ script.js
@@ -1,54 +1,65 @@
-// Requirements 
-// Aan deze eisen moet jouw project voldoen:
-
-// 1.Als gebruiker wil ik een lijst kunnen zien van de beschikbare films met de poster van de film. Check de data: elke film in de database heeft een link naar de juiste poster.
-// 2.Als gebruiker wil ik bovenaan de pagina kunnen klikken op 5 verschillende filters in de vorm van radio-buttons. De filter functionaliteit wordt hieronder verder toegelicht.
-// 3.Als gebruiker kan ik maar 1 filter tegelijk gebruiken.
-// Als ik een ander filter aanklik gaat het andere filter dus weer uit. (Hence de radiobutton (1 antwoord mogelijk), in tegenstelling tot een checkbox (meerdere antwoorden mogelijk).
-// 4.Categorie 1: Als gebruiker wil ik kunnen filteren op de categorie nieuwste films: van de laatste jaren, dat betekent 2014 of nieuwer.
-// 5.Categorie 2: Als gebruiker wil ik kunnen filters op films met "Avengers" in de titel.
-// 6.Categorie 3: Als gebruiker wil ik kunnen filteren op films met "X-Men" in de titel.
-// 7.Categorie 4: Als gebruiker wil ik kunnen filteren op films met "Princess" in de titel.
-// 
-//8.Categorie 5: Als gebruiker wil ik kunnen filteren op films met "Batman" in de titel.
-// Gebruik arraymethods voor je filters
-// Check of een gedeelte van een string in een andere string aanwezig is met de .includes() method.
-// De laatste 4 filters lijken heel veel op elkaar. Heb je daar meerdere functies voor nodig? Kan het ook in 1?
-// 
-// 9.Als gebruiker kan ik op de poster van de film klikken, waardoor ik naar de juiste IMDB pagina wordt gebracht.
-// IMDB werkt met een id per film/serie in de URL. Deze ids vind je ook weer terug in onze filmdatabase. Zie bijvoorbeeld: https://www.imdb.com/title/tt0944947/ Pas de URL van IMDB aan en plak het juiste ID erachter.
-
-
-
-// function check (){
-//     document.getElementById("new").checked = true;
-// }
-
-// function uncheck (){
-//     document.getElementById("new").checked = false;
-// }
-
-// console.log(movies);
-
 const movieList = document.getElementById ("movie-list");
+const radioButtons = document.querySelectorAll(".radio");
 
-// let addMoviesToDom = movies.map (movie => {
-//     const movieName = movie.Title
-//     const newLi = document.createElement ("li");
-//     newLi.appendChild(document.createTextNode(movieList));
-//     newLi.innerHTML = movieName;
-//     movieList.appendChild(newLi);
-//     return movie.Title;
-// });
+const addMoviesToDom = (movies) =>{
+    const listedMovies = movies.map (movie => {
+        const movieLi = document.createElement ("li");
+        const posterLink = document.createElement ("a");
+        const moviePoster = document.createElement ("img");
+        movieLi.classList.add('movie__li');
+        posterLink.classList.add('poster__link');
+        moviePoster.classList.add('movie__poster');
+        moviePoster.src = movie.Poster;
+        movieList.appendChild(movieLi);
+        movieLi.appendChild(posterLink);
+        posterLink.appendChild(moviePoster);
+    });
+};
+addMoviesToDom(movies);
 
-let addMoviesToDom = movies.map (movie => {
-    const moviePoster = document.createElement ("img");
-    const posterA = document.createElement ("a");
-    // posterA.classList.add('movie__poster');
-    moviePoster.src = movie.Poster;
-    movieList.appendChild(posterA);
-    posterA.appendChild(moviePoster);
-    return movie.Title;
-});
+const eventListeners = () => {
+    Array.from(radioButtons).forEach ((radio) => {
+        radio.addEventListener ('change', (event) => {
+        handleOnChangeEvent(event.target.id)
+        });
+    });
+}
+eventListeners(radioButtons);
 
-console.log(addMoviesToDom);
+const handleOnChangeEvent = (event) => {
+    let filterMovies = movies;
+    switch (event) {
+            case 'x-men':
+                console.log("ik ben een X-Men film")
+                filterMovies = movies.filter (movie => movie.Title.includes('X-Men'));
+                console.log(filterMovies);
+                break;
+            case 'avengers':
+                console.log("ik ben een Avengers film")
+                filterMovies = movies.filter (movie => movie.Title.includes('Avengers'));
+                console.log(filterMovies);
+                break;
+            case 'batman':
+                console.log("ik ben een Batman film")
+                filterMovies = movies.filter (movie => movie.Title.includes('Batman'));
+                console.log(filterMovies);
+                break;
+            case 'princess':
+                console.log("ik ben een Princess film")
+                filterMovies = movies.filter (movie => movie.Title.includes('Princess'));
+                console.log(filterMovies);
+                break;
+            case 'new': 
+                console.log("ik ben een nieuwe film")
+                filterMovies = movies.filter (movie => movie.Year > 2014)
+                console.log(filterMovies);
+                break;
+            default:
+                addMoviesToDom;
+    }
+    const empty = () => {
+        movieList.innerHTML = ' ';
+    };
+    empty();
+    addMoviesToDom(filterMovies);
+}
